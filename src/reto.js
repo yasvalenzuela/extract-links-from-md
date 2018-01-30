39038f8bab23f024632f2cdb16488cabd7ca7497
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-01-30 20:42:47 -0300

    modificando funciones y test

--- src/reto.js
+++ src/reto.js
@@ -1,7 +1,74 @@
-let URL =  new url();
+
+
+'use strict';
+const URL = {};
 const http = require('http');
 // const readline = require("readline");
 const fs = require('fs');
+
+
+URL.textAndLink = () => {
+  fs.readFile('markdown.md', 'utf8', (err, text) => {
+    const regularExpression = /(\[(.*?)\]\([^"'\s]+)/g;
+    let read = text.match(regularExpression).toString();
+    return read;
+  // console.log(read);
+  });
+};
+
+
+URL.searchLink = (text) => {
+  fs.readFile('markdown.md', 'utf8', (err, text) => {
+    const regularExpression = /([^"'\s]+)/g;
+    let links = text.match(regularExpression).toString();
+    return links;
+  });
+};
+
+/*
+URL.searchLink = (text) => {
+  const regularExpression = /([^"'\s]+)/g;
+  let links = text.match(regularExpression).toString();
+  return links;
+  // console.log(URL.searchLink);
+};
+*/
+
+/*
+let searchText = fs.readFile('markdown.md', 'utf8', (err, text) => {
+  const regularExpression = /(\[(.*?)\]\([^"'\s]+)/g;
+  let reads = text.match(regularExpression).toString();
+  return reads;
+  // console.log(reads);
+});
+*/
+
+URL.searchText = (text) => {
+  const regularExpression = /(\[(.*?)\])/g;
+  let texts = text.match(regularExpression).toString();
+  if (texts !== '') {
+    return texts;
+  }
+};
+
+
+URL.searchLinkTotal = (text) => {
+  let urlTotal = text.map((element) => {
+    let searchLink = URL.searchLink(element);
+    let searchText = URL.searchText(element);
+    let object = {
+      href: url,
+      text: text};
+    return object;
+    // console.log(object);
+  });
+  return urlTotal;
+  // console.log(total);
+};
+
+module.exports = URL;
+
+
 
 /*
 * let urls = readline.createInterface({
@@ -10,19 +77,42 @@ const fs = require('fs');
 * });
 */
 
-fs.readFile('markdown.md', 'utf8', (err, text) => {
-  const links = /(\[(.*?)\]\([^"'\s]+)/g;
-  let read = text.match(links);
-  console.log(read);
-});
+/*
+URL.searchLink = (txt) => {
+  const regularExpression = /([^"'\s]+)/g;
+  let links = link.match(regularExpression);
+  return links;
+};
 
-class url {
-  constructor(link, text) {
-    this.href = link;
-    this.text = text;
+
+URL.searchText = (txt) => {
+  const regularExpression = /(\[(.*?)\])/g;
+  let text = txt.match(regularExpression);
+  if (text !== '') {
+    return text;
   }
-}
-console.log(URL);
+};
+
+
+URL.results = (txt) => {
+  let content = fs.readFile('markdown.md', 'utf8', (err, text) => {
+    const regularExpression = /(\[(.*?)\]\([^"'\s]+)/g;
+    let read = text.match(regularExpression).toString();
+  console.log(read);
+  });;
+  let total = links.map(() => {
+    let url = URL.url();
+    let text = URL.text();
+    let object = {
+      href: url,
+      text: text};
+    return object;
+    console.log(object);
+  });
+  return total;
+  console.log(total);
+};
+
 // /(\[(.*?)\])/g = esta expresion regular reconoce texto entre [];
 // /([^"'\s]+)/g = esta expresion regular reconoce los url que estan dentro del texto markdown
 
@@ -41,9 +131,10 @@ console.log(URL);
 */
 
 
-// module.exports = URL;
+/*
 
 // text = (markdown.md),
 // encontrados = text.match(links);
 
 // console.log(encontrados)
+*/
--- test/test.js
+++ test/test.js
@@ -1,2 +1,32 @@
-var assert = require("assert");
-var url = require('../src/reto.js');
+
+const assert = require('assert');
+const chai = require('chai');
+const URL = require('../src/reto.js');
+let text = `# Lorem ipsum
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et
+[dolore](https://en.wiktionary.org/wiki/dolore) magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat.
+[foo](http://foo.com)`;
+
+describe('comprobar que detecta URL', () => {
+  it('Debe comprobar que devuelva las URL en ()', () => {
+    chai.assert.equal(URL.searchLink(text), ('(https://en.wiktionary.org/wiki/labore),(http://en.wiktionary.org/wiki/labore),(http://foo.com)'), true);
+    // chai.assert.equal(URL.searchLink(text), 'google', false);
+  });
+});
+
+describe('comprobar que detecta texto en []', () => {
+  it('Debe comprobar que devuelva el texto en []', () => {
+    chai.assert.equal(URL.searchText(text), ('[labore],[dolore],[foo]'), true);
+    // chai.assert.equal(URL.searchText(text), ('['']'), false);
+  });
+});
+
+describe('comprobar que devuelva un objeto de arreglos', () => {
+  it('Debe comprobar que devuelva {href: http://foo.com, text: text}', () => {
+    chai.assert.equal(URL.searchLinkTotal(text), '', true);
+    // chai.assert.equal(URL.searchText(text), 'google', false);
+  });
+});
