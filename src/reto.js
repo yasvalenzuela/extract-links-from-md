5c3b759326bd36353c49accaae058f587fcfb556
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-02-01 11:31:45 -0300

    funciones

--- src/reto.js
+++ src/reto.js
@@ -2,29 +2,58 @@
 
 'use strict';
 const URL = {};
-const http = require('http');
+// const http = require('http');
 // const readline = require("readline");
 const fs = require('fs');
+let texto = fs.readFileSync('./markdown.md').toString();
 
 
-URL.textAndLink = () => {
+URL.extractLinksAndTexts = (texto) => {
+  const regularExpression = /\[(.*?)\](([^\s]+))/gi;
+  let linksAndTexts = texto.match(regularExpression).toString();
+  return linksAndTexts;
+  console.log(linksAndTexts);
+};
+
+// URL.extractLinksAndTexts(texto);
+
+URL.extractLinks = (texto) => {
+  const regularExpression = /(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi;
+  let links = texto.match(regularExpression).toString();
+  return links;
+  console.log(links);
+};
+
+//URL.extractLinks(texto);
+
+URL.extractTexts = (texto) => {
+  const regularExpression = /\[(.*?)\]/g;
+  let txt = texto.match(regularExpression).toString();
+  return txt;
+  console.log(txt);
+};
+
+
+// URL.extractTexts(texto);
+/*
+URL.textAndLink = (err, text) => {
   fs.readFile('markdown.md', 'utf8', (err, text) => {
     const regularExpression = /(\[(.*?)\]\([^"'\s]+)/g;
     let read = text.match(regularExpression).toString();
     return read;
-  // console.log(read);
+  console.log(read);
   });
 };
 
 
-URL.searchLink = (text) => {
+URL.searchLink = (err, text) => {
   fs.readFile('markdown.md', 'utf8', (err, text) => {
     const regularExpression = /([^"'\s]+)/g;
     let links = text.match(regularExpression).toString();
     return links;
   });
 };
-
+*/
 /*
 URL.searchLink = (text) => {
   const regularExpression = /([^"'\s]+)/g;
@@ -49,6 +78,7 @@ URL.searchText = (text) => {
   if (texts !== '') {
     return texts;
   }
+  console.log(texts);
 };
 
 
