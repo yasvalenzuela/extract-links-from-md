5ef434b721de226ca74ec8f9cd0212555c7f9f0e
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-02-01 22:05:41 -0300

    modificando funciones y test

--- src/reto.js
+++ src/reto.js
@@ -7,47 +7,46 @@ let texto = fs.readFileSync('./markdown.md').toString();
 // console.log(texto);
 
 
-URL.extractLinksAndTexts = (texto) => {
-  const regularExpression = /\[(.*?)\](([^\s]+))/gi;
-  let linksAndTexts = texto.match(regularExpression).toString();
-  return linksAndTexts;
-  console.log(linksAndTexts);
-};
-
-// URL.extractLinksAndTexts(texto);
-
 URL.extractLinks = (texto) => {
-  const regularExpression = /(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi;
-  let links = texto.match(regularExpression).toString();
+  const regularExpression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  let links = texto.match(regularExpression);
   return links;
-  console.log(links);
+  // console.log(links);
 };
 
-/*URL.extractLinks(texto);
+// URL.extractLinks(texto);
 
 URL.extractTexts = (texto) => {
   const regularExpression = /\[(.*?)\]/g;
-  let txt = texto.match(regularExpression).toString();
-  if (txt !== '') {
-    return txt;
-  }
+  let txt = texto.match(regularExpression);
+  return txt;
   console.log(txt);
 };
 
-
 // URL.extractTexts(texto);
-/*
-URL.textAndLink = (err, text) => {
-  fs.readFile('markdown.md', 'utf8', (err, text) => {
-    const regularExpression = /(\[(.*?)\]\([^"'\s]+)/g;
-    let read = text.match(regularExpression).toString();
-    return read;
-  console.log(read);
-  });
-};*/
 
+
+let linksTotal = URL.extractLinks(texto);
+let txtTotal = URL.extractTexts(texto);
+URL.extractLinksAndTexts = (texto) => {
+  let url = [];
+  for (let i = 0; i < linksTotal.length; i++) {
+    for(let j = 0; j < txtTotal.length; j++) {
+      url.push(
+      {href: linksTotal[i],
+      text: txtTotal[j] }
+      );
+    i++;
+    j++;
+    }
+    
+  }
+  return url;
+  // console.log(url);
+};
+
+URL.extractLinksAndTexts(texto);
 
 
 module.exports = URL;
-
 
--- test/test.js
+++ test/test.js
@@ -1,26 +1,39 @@
 const assert = require('assert');
 const chai = require('chai');
 const expect = chai.expect;
-const URL = require('../src/extract-links-from-md.js');
+const URL = require('../src/reto.js');
 const fs = require('fs');
-let texto = fs.readFileSync('./markdown.md').toString()
+let texto = fs.readFileSync('./markdown.md').toString();
 
 
 describe('Comprobar que extrae links', () => {
   it('Debe retornar links de un texto', () => {
     chai.assert.equal(URL.extractLinks(texto), ('https://es.wikipedia.org/wiki/Idioma_ingl%C3%A9s,https://es.wikipedia.org/wiki/Drama,https://es.wikipedia.org/wiki/Verso,https://es.wikipedia.org/wiki/Romanticismo'
-), true);
-    //chai.assert.equal(URL.extractLinks, ('[http://www.google.cl]'), undefined);
-    //expect(URL.extractLinks.to.be.equal('http://www.google.cl'));
+    ), true);
+    // chai.assert.equal(URL.extractLinks, ('[http://www.google.cl]'), undefined);
+    // expect(URL.extractLinks.to.be.equal('http://www.google.cl'));
   });
 });
 
 describe('Comprobar que extrae texto', () => {
   it('Debe retornar texto que se encuentra en []', () => {
     chai.assert.equal(URL.extractTexts(texto), ('[ingles],[drama],[verso],[románticos]'), true);
-    //chai.assert.equal(URL.extractTexts, ('(palabra)'), false);
+    // chai.assert.equal(URL.extractTexts, ('(palabra)'), false);
     // expect(URL.extractText(text)).to.be.equal('[palabra]');
   });
 });
 
 
+
+
+describe('Comprobar que devuelve un arreglo de objetos', () => {
+  it('Debe retornar un arreglo de objetos', () => {
+    chai.assert.equal(URL.extractLinksAndTexts(texto), ([
+      {href: 'https://es.wikipedia.org/wiki/Idioma_ingl%C3%A9s', text: '[ingles]'},
+      {href: 'https://es.wikipedia.org/wiki/Drama', text: '[drama]'},
+      {href: 'https://es.wikipedia.org/wiki/Verso', text: '[verso]'},
+      {href: 'https://es.wikipedia.org/wiki/Romanticismo', text: '[románticos]'}
+    ]), true);
+    // expect(URL.extractLinksAndText(texto)).to.be.equal('[{href: "https://es.wikipedia.org/wiki/Drama", text: "[ingles]"},{href: "https://es.wikipedia.org/wiki/Drama", text: "[drama]"},{href: "https://es.wikipedia.org/wiki/Verso", text: "[verso]"},{href: "https://es.wikipedia.org/wiki/Romanticismo", text: "[románticos]"}]');
+  });
+});
